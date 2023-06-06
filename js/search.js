376f318114181a2dfb1132b81518c68a5114fef8
ncgh-00 <nguyencaogiahuy@gmail.com>
2023-06-06 15:48:40 +0700

    commit search page

---
+++ js/search.js
@@ -0,0 +1,23 @@
+const rangeInput = document.querySelectorAll(".search-wrapper .price-filter .range-input input"),
+priceInput = document.querySelectorAll(".price-filter .price-input .field input"),
+range = document.querySelector(".search-wrapper .price-filter .slider .progress");
+let priceGap = 100;
+
+rangeInput.forEach(input =>{
+    input.addEventListener("input", e =>{
+        let minVal = parseInt(rangeInput[0].value),
+        maxVal = parseInt(rangeInput[1].value);
+        if((maxVal - minVal) < priceGap){
+            if(e.target.className === "range-min"){
+                rangeInput[0].value = maxVal - priceGap
+            }else{
+                rangeInput[1].value = minVal + priceGap;
+            }
+        }else{
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+            range.style.left = ((minVal / rangeInput[0].max) * 100) + "%";
+            range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+        }
+    });
+});
