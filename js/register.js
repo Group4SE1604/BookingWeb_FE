84015ba80a6cf71f2fb80fcc7e3dec27949e0611
ncgh-00 <nguyencaogiahuy@gmail.com>
2023-06-12 23:17:00 +0700

    update js

--- js/register.js
+++ js/register.js
@@ -3,25 +3,31 @@ $('.owl-register').owlCarousel({
     dots: true,
     items: 1,
     center: true,
-    margin: 0, 
+    margin: 0,
     autoplay: true
 });
 
 const pwShowHide = document.querySelectorAll(".eye-icon"),
-pwFields = document.querySelectorAll(".input-pass")
+    pwFields = document.querySelectorAll(".input-pass")
 
 pwShowHide.forEach(eyeIcon => {
     eyeIcon.addEventListener("click", () => {
-
-        pwFields.forEach(password => {
-            if (password.type === "password") {
-                password.type = "text";
+        if (eyeIcon === pwShowHide[0]) {
+            if (pwFields[0].type === "password") {
+                pwFields[0].type = "text";
                 eyeIcon.classList.replace("fa-eye-slash", "fa-eye");
                 return;
             }
-            password.type = "password";
+            pwFields[0].type = "password";
             eyeIcon.classList.replace("fa-eye", "fa-eye-slash");
-        })
-
-    })
+        } else if (eyeIcon === pwShowHide[1]) {
+            if (pwFields[1].type === "password") {
+                pwFields[1].type = "text";
+                eyeIcon.classList.replace("fa-eye-slash", "fa-eye");
+                return;
+            }
+            pwFields[1].type = "password";
+            eyeIcon.classList.replace("fa-eye", "fa-eye-slash");
+        }
+    });
 })
